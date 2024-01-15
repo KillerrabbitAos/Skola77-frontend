73ae15a72b1911624cfe937c859772c93a82a10d
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-01-15 13:44:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function calculateFontSize = (container, text, baseFontSize = 16) {
   const containerWidth = container.offsetWidth;
   const elementWidth = container.offsetWidth;
   const ratio = containerWidth / elementWidth;
+  alert(baseFontSize * ratio)
   return baseFontSize * ratio;
 }
 
