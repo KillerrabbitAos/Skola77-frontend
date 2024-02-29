31937012fdc03a6907d120786a416f139163b39e
Bo Gustavsson <bosse@etm571.com>
2024-02-29 20:42:19 +0100

    Nevermind, såg nu

--- src/schackVärden.js
+++ src/schackVärden.js
@@ -1,5 +1,4 @@
 //ett område att bygga på. Implementera olika färger, bilder för pjäser etc. Men lägsta prio såklart. Det är också lånngt ifrån klart så håll det hemligt.
-//Hur kommer man till dit?
 
 const schackBräde = {
     "rows": 8,
