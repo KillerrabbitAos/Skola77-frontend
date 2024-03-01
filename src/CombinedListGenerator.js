90ac2f2ebbc17ae511b642cc7deb94b00b652470
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-03-01 20:47:28 +0100

    fix!

--- src/CombinedListGenerator.js
+++ src/CombinedListGenerator.js
@@ -1,12 +1,13 @@
 const generateCombinedList = (list2, list4, defaultValue, names, låstaNamn, boxNames) => {
   const maxUniqueNumber = Math.min(list4.length - 1, list2.length);
   const combinedList = [];
-  const shuffledList2 = []
+  const shuffledList = []
   const addLater = []
   const list4DeepCopy = JSON.parse(JSON.stringify(list4));
   const list3 = shuffleArray(list4DeepCopy);
   const shuffledList2Deep = JSON.parse(JSON.stringify(shuffleArray(list2)));
-  
+  const usedNumbers = []
+
 console.log(list2)
   for (let i = 0; i < list2.length; i++){
   if (låstaNamn.includes(list2[i])) {
@@ -15,32 +16,34 @@ console.log(list2)
     if (foundItem){
       console.log("orm")
       addLater.push(foundItem)
-     // usedIndexes.add(JSON.parse(JSON.stringify(foundItem.value)))
+      usedNumbers.push(JSON.parse(parseInt(JSON.stringify(parseInt(foundItem.value)))))
+     //console.log(usedIndexes)
       console.log(foundItem)
     }
   }
   else{
-  shuffledList2.push(list2[i])
+  shuffledList.push(list2[i])
   }
 }
-let usedIndexes = new Set();
+let usedIndexes = new Set(); 
+
 console.log(usedIndexes)
-console.log(shuffledList2)
-shuffleArray(shuffledList2)
+
+const shuffledList2 = shuffleArray(shuffledList)
 
   let i = 0;
-  for (; i < list3.length && combinedList.length < shuffledList2.length; i++) {
+  for (; i < list4.length && combinedList.length < shuffledList2.length; i++) {
     const key = shuffledList2[combinedList.length];
 
     let randomIndex = Math.floor(Math.random() * names.length);
-    while (usedIndexes.has(randomIndex)) {
+    while (usedIndexes.has(randomIndex) ) {
       randomIndex = Math.floor(Math.random() * names.length);
     }
     usedIndexes.add(randomIndex);
 
     let value = randomIndex;
 
-    if (names[value] !== "" && !låstaNamn.includes(value)) {
+    if (names[value] !== "" && !låstaNamn.includes(value) && !usedNumbers.includes(value)) {
       combinedList.push({ key, value });
     }
 
