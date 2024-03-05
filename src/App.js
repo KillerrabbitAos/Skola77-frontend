8eb59838ca538204a54a44dd20b65a150884f53b
Bo Gustavsson <bosseg09@gmail.com>
2024-03-05 20:21:17 +0100

    tog bort boxnamn() från borttagna boxar + sparar låsta boxar i cookie

--- src/App.js
+++ src/App.js
@@ -18,8 +18,10 @@ function compressData(data) {
 }
 
 function orm() {
+  //bästa funktionen 2024
   alert(orm);
 }
+
 // Function to decompress data retrieved from cookies
 function decompressData(compressedData) {
   return JSON.parse(LZString.decompressFromEncodedURIComponent(compressedData));
@@ -130,6 +132,7 @@ const App = () => {
         cellSize: cellSize,
         fixaCounter: fixaCounter,
         keyChange: keyChange,
+        låstaNamn: låstaNamn,
       });
 
       Cookies.set(`${name}_values`, compressedData, { expires: 365 });
@@ -298,6 +301,8 @@ const App = () => {
       setFilledBoxes([]);
       setCellSize(70);
       setFixaCounter(0);
+      setLåstaNamn([]);
+
     } else {
       var values = schackBräde
       nere = ("Svart")
@@ -324,6 +329,7 @@ const App = () => {
         setGroupName(selectedGroup.replace('_values', ''));
         setUppe(uppe);
         setNere(nere);
+        setLåstaNamn(values.låstaNamn || []);
       } else {
         // Handle the case when values are not available
         console.error(`No values found for group: ${selectedGroup}`);
