883a682ad86912cedda74a528d9378166749e263
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-03-11 19:54:22 +0100

    update

--- src/sidor/NoPage.js
+++ src/sidor/NoPage.js
@@ -1,5 +1,11 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <div>
+        
+    <h1>404</h1>
+    <h2>Sidan du söker verkar inte finnas. Kolla i addressfönstret efter felstavningar eller liknande.</h2>
+    </div> 
+    )
   };
   
   export default NoPage
