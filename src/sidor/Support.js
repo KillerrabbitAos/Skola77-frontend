a13115bcbb9fe979320802bd720502a0e88eec8b
Bo Gustavsson <bosseg09@gmail.com>
2024-03-13 21:55:35 +0100

    frågor och svar

--- src/sidor/Support.js
+++ src/sidor/Support.js
@@ -30,6 +30,29 @@ const Support = () => {
   
   
       </div>
+
+      <hr/>
+
+      <div>
+
+        <h1 id="frågorOchSvarHead">Vanliga frågor och svar:</h1>
+
+        <div className="frågorOchSvar">
+
+        <p id="fråga">Kan jag spara mina placeringar utan att behöva ladda ner dem?</p>
+        <p id="svar">Ja, det kan du. Klicka på sparaikonen högt upp på sidan och skriv in ett namn på placeringen.</p>
+
+        <p id="fråga">Kan jag spara klassrum och klasslistor separat?</p>
+        <p id="svar">Ja, det finns två "spararutor" utplacerade vid klassrumsskaparen och vid namnlistan längst ned. Mer info finns i snabbguiden vid punkt 1 och 2.</p>
+
+        <p id="fråga">Jag har hittat ett fel i programmet, vad ska jag göra?</p>
+        <p id="svar">Om du har hittat ett fel eller problem, tveka inte att kontakta oss på <a id="mail" href="mailto:feedback@skola77.com">feedback@skola77.com</a> så hjälper vi dig vidare därifrån.</p>
+
+
+        </div>
+
+
+      </div>
       
   </body>);
   };
