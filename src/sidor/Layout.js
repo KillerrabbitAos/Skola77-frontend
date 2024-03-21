bbaabb6d9973923ddb7c62bd557ca4c7289c5b3a
Bo Gustavsson <bosse@etm571.com>
2024-03-21 07:52:58 +0100

    Update Layout.js

--- src/sidor/Layout.js
+++ src/sidor/Layout.js
@@ -8,7 +8,7 @@ const Layout = () => {
           <li className="header" id="main">
             <NavLink to="/" activeClassName="active">
               
-              <img src="/Skola77.png" id="kebbe"></img>
+              <img src="/nyLogga.png" id="kebbe"></img>
 
             </NavLink>
           </li>
