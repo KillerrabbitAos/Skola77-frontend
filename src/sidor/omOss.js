c22d3ef9c5fcbc92623f4a30c280e22acf2bfaf3
Bo Gustavsson <bosseg09@gmail.com>
2024-05-14 22:16:11 +0200

    kommer snart på omoss

--- src/sidor/omOss.js
+++ src/sidor/omOss.js
@@ -12,12 +12,8 @@ const OmOss = () => {
 
         <div id="bodyOmOss">
 
-
-        Välkommen till Skola77, där innovation möter passion för att förbättra skolmiljön! Vi är inte bara en bordsplaceringshemsida, vi är en idé född ur vårt eget behov av att göra skolan till en mer inkluderande och givande plats för alla elever. Vi är inte bara en grupp unga killar, vi är visionärer som strävar efter att göra verklig skillnad.
-Vår Berättelse: januari så kom en av våra programmerare Artur på idén om att skapa ett nytt bordsplaceringsprogram för lärare. Han började jobba under jullovet på det som skulle utvecklas till Skola77. Till hjälp med programmeringen rekryterades kompisen Bosse. Oscar fick senare bli säljare och Sam fick bli it support.
-Vår Mission: Vår mission är att göra det enklare och smidigare för skolor att skapa en trivsam och givande lärandemiljö genom att erbjuda en intuitiv och automatiserad plattform för bordsplaceringshantering. Vi strävar efter att främja samarbete, öppenhet och inkludering genom vår tjänst, vilket ger både elever och lärare en bättre skolupplevelse.
-Vår Vision: Vi ser en framtid där varje skola har tillgång till verktyg som gör det möjligt för dem att optimera sin bordsplaceringsprocess för att främja positiva relationer, öka lärandet och stärka gemenskapen. Genom att ständigt förbättra och utveckla vår plattform strävar vi efter att vara ledande inom detta område och inspirera andra att omfamna innovation för att skapa bättre utbildningsmiljöer.
-Varför Välja Oss: När du väljer Skola77 väljer du inte bara en bordsplaceringshemsida, du väljer en partner som delar din passion för att göra skolan till en bättre plats. Vi är unga, vi är entusiastiska och vi är fast beslutna att göra en verklig skillnad. Ge oss chansen att hjälpa dig att skapa en mer inkluderande och trivsam skolmiljö - tillsammans kan vi göra det möjligt!
+        <h2><u>Kommer snart...</u></h2>
+    
 
         </div>
 
