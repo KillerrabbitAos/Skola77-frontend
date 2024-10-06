9b99e4ab0584bc28fb8f5681eac77576a62b265c
Bo Gustavsson <bosseg09@gmail.com>
2024-10-06 14:40:15 +0200

    Omoss Fixes #109

--- src/sidor/omOss.js
+++ src/sidor/omOss.js
@@ -12,8 +12,43 @@ const OmOss = () => {
 
         <div id="bodyOmOss">
 
-        <h2><u>Skola77 är ett bordsplaceringsprogram utvecklat av en grupp ungdomar. Vi tycker det är självklart att lärare ska ha tillgång till välgjorda och effektiva verktyg för att kunna skapa trygghet och arbetsro i klassrummen. Det är därför vårt bordsplaceringsprogram är utvecklat med lärare i åtanke. Dessutom är vår hemsida helt gratis och utan reklam. 
-</u></h2>
+        <h2>Skola77 är ett bordsplaceringsprogram utvecklat av en grupp ungdomar. Vi tycker det är självklart att lärare ska ha tillgång till välgjorda och effektiva verktyg för att kunna skapa trygghet och arbetsro i klassrummen. Det är därför vårt bordsplaceringsprogram är utvecklat med lärare i åtanke. Dessutom är vår hemsida helt gratis och utan reklam. 
+</h2>
+
+
+<div class="users-container">
+    <div class="user-container">
+        <div class="user-icon">A</div>
+        <div class="user-info">
+            <div class="user-name">Artur Norrby</div>
+            <div class="user-role">Utvecklare</div>
+        </div>
+    </div>
+
+    <div class="user-container">
+        <div class="user-icon">B</div>
+        <div class="user-info">
+            <div class="user-name">Bo Gustavsson</div>
+            <div class="user-role">Utvecklare</div>
+        </div>
+    </div>
+
+    <div class="user-container">
+        <div class="user-icon">O</div>
+        <div class="user-info">
+            <div class="user-name">Oscar Brännström</div>
+            <div class="user-role">Arbetare</div>
+        </div>
+    </div>
+
+    <div class="user-container">
+        <div class="user-icon">S</div>
+        <div class="user-info">
+            <div class="user-name">Samuel Nordin</div>
+            <div class="user-role">Arbetare</div>
+        </div>
+    </div>
+</div>
     
 
         </div>
