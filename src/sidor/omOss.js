0f3a1bd3859c805f9683860d46436ca01000296c
zom <156358008+zombifies@users.noreply.github.com>
2024-10-06 14:19:17 +0200

    om oss text

--- src/sidor/omOss.js
+++ src/sidor/omOss.js
@@ -12,7 +12,8 @@ const OmOss = () => {
 
         <div id="bodyOmOss">
 
-        <h2><u>Kommer snalrt...</u></h2>
+        <h2><u>Skola77 är ett bordsplaceringsprogram utvecklat av en grupp ungdomar. Vi tycker det är självklart att lärare ska ha tillgång till välgjorda och effektiva verktyg för att kunna skapa trygghet och arbetsro i klassrummen. Det är därför vårt bordsplaceringsprogram är utvecklat med lärare i åtanke. Dessutom är vår hemsida helt gratis och utan reklam. 
+</u></h2>
     
 
         </div>
