922416f0eaa80d22f0a7c030d2738fb21ec03bc9
Artur <artur.norrby@gmail.com>
2024-11-13 18:07:52 +0100

    progress

--- src/Namn.js
+++ src/Namn.js
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ExcelToTextConverter from "./ExcelToTextConverter";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { isMobile, isTablet } from "react-device-detect";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
-import { data as initialData } from "./data";
+import { data } from "./data";
 
 const NameList = ({}) => {
+  const [namn, setNamn] = useState([data.klasser[0].personer]);
+  const textrutaRef = useRef(null);
+  const läggTillNamn = () => {
+    textrutaRef.current && setNamn(prevNamn => [...prevNamn,s textrutaRef.current.value]);
+  };
   return (
     <div>
       <div className="flex">
         <div className="aspect-square w-[25vw]"></div>
         <textarea
+          ref={textrutaRef}
           className="aspect-[2/1] w-[50vw]"
           placeholder={`Ett namn per rad:
 Artur
@@ -19,8 +25,14 @@ Sam
 etc...
 `}
         ></textarea>
-        <div className="aspect-square w-[25vw] flex flex-row justify-center items-center text text-[5vw]">Lägg till</div>
+        <div
+          className="aspect-square w-[25vw] flex flex-row justify-center items-center text text-[5vw]"
+          onClick={läggTillNamn}
+        >
+          Lägg till
+        </div>
       </div>
+      {namn.map(namn => <div>namn</div>)}
     </div>
   );
 };
