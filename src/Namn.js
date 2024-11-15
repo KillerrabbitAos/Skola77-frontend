6503c0fd35dc4a5946e52e0017e35168376d4882
Artur <artur.norrby@gmail.com>
2024-11-15 10:12:33 +0100

    fungerar nu

--- src/Namn.js
+++ src/Namn.js
@@ -4,10 +4,9 @@ function divideArray(array, delar) {
   const parts = delar < array.length ? delar : array.length;
   let result = [];
   let partSize = Math.floor(array.length / parts);
-  let remainder = array.length % parts; 
+  let remainder = array.length % parts;
 
   for (let i = 0; i < parts; i++) {
-   
     let extra = i < remainder ? 1 : 0;
     result.push(
       array.slice(
@@ -23,7 +22,9 @@ function divideArray(array, delar) {
 const NameList = ({}) => {
   const [namn, setNamn] = useState([""]);
   const textrutaRef = useRef(null);
+  const [visaLaddaKlassrum, setVisaLaddaKlassrum] = useState(false);
   const [kolumner, setKolumner] = useState(10);
+  const [klassnamn, setKlassnamn] = useState(null)
   const läggTillNamn = () => {
     setNamn((prevNamn) => [
       ...prevNamn,
@@ -65,7 +66,7 @@ const NameList = ({}) => {
             </div>
           </div>
         )),
-      Math.floor(window.outerWidth / 320)
+      Math.floor(window.outerWidth / 220)
     );
   const namnLista =
     namn &&
@@ -117,9 +118,48 @@ const NameList = ({}) => {
 
   return (
     <div>
+      {visaLaddaKlassrum && (
+        <div
+          style={{
+            position: "absolute",
+            top: "45vh",
+            left: "45vw",
+            width: "10vw",
+            height: "10vw",
+            backgroundColor: "white"
+          }}
+        >
+          {data.klasser
+            .slice()
+            .reverse()
+            .map((klass) => (
+              <li
+                key={klass.namn}
+                className="font-bold text-xl p-2 cursor-pointer"
+                onClick={() => {
+                  setNamn(klass.personer);
+                  setKlassnamn(klass.namn);
+                  setVisaLaddaKlassrum(false)
+                }}
+              >
+                {klass.namn}
+              </li>
+            ))}
+        </div>
+      )}
       <div className="flex">
-        <div className="aspect-square w-[25vw] flex cursor-pointer flex-row text-[5vw] justify-center items-center">
-          Spara
+        <div>
+          <div className="bg-[#4CAF50] border h-[12.5vw] text-white w-[25vw] flex cursor-pointer flex-row text-[5vw] justify-center items-center">
+            Spara
+          </div>
+          <div
+            onClick={() => {
+              setVisaLaddaKlassrum(!visaLaddaKlassrum);
+            }}
+            className="bg-[#4CAF50] border h-[12.5vw] text-white w-[25vw] flex cursor-pointer flex-row text-[5vw] justify-center items-center"
+          >
+            Ladda
+          </div>
         </div>
         <textarea
           ref={textrutaRef}
@@ -132,7 +172,7 @@ etc...
 `}
         ></textarea>
         <div
-          className="cursor-pointer aspect-square w-[25vw] flex flex-row justify-center items-center text-[5vw]"
+          className="cursor-pointer aspect-square border bg-[#4CAF50] text-white w-[25vw] flex flex-row justify-center items-center text-[5vw]"
           onClick={läggTillNamn}
         >
           Lägg till
