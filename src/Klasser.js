3ba42d15d3bcc93f49f35e1c2b4f1a7b17d363e7
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-11-17 11:56:59 +0100

    gömmer scrollbaren

--- src/Klasser.js
+++ src/Klasser.js
@@ -44,7 +44,7 @@ const Klasser = ({}) => {
             key={namnObj.orginalIndex}
             className="bg-white w-[200px]  h-[40px] m-1 border flex flex-row justify-start items-center"
           >
-            <div className="text-[20px] ml-[1vw] overflow-x-scroll w-[90%]">
+            <div className="text-[20px] ml-[1vw] overflow-x-scroll scrollbar-none w-[90%]">
               <div>{namnObj.namn}</div>
             </div>
 
