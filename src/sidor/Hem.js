3e4dd448732a6a14553db35a246628aed68b29dd
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-11-20 16:34:55 +0100

    Update Hem.js

--- src/sidor/Hem.js
+++ src/sidor/Hem.js
@@ -3,13 +3,6 @@ import React, { useEffect } from "react";
 import { isMobile, isTablet } from "react-device-detect";
 
 const Hem = () => {
-  useEffect(() => {
-    if (isMobile && !isTablet) {
-      alert(
-        "Du besöker just nu Skola77 från en mobiltelefon. Observera att sidan inte alls är utformad för den typen av användande. Vi rekommenderar starkt att du testar sidan på dator för en bättre användarupplevelse."
-      );
-    }
-  }, []);
 
   return (
     <div style={{ 
@@ -17,7 +10,7 @@ const Hem = () => {
         justifyContent: 'center', 
         alignItems: 'center', 
         height: '100vh', 
-        userSelect: 'none'  // Prevent text selection
+        userSelect: 'none'
     }}>
       <h1>Skola77 2 alfa</h1>{"\n"}
       <p>20241031</p>
