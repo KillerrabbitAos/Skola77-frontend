2098d1feb0bdf5f220db8675c603019f749b1928
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-11-20 19:37:38 +0100

    styling

--- src/sidor/Layout.js
+++ src/sidor/Layout.js
@@ -8,7 +8,7 @@ import "./Layout.css";
 const Layout = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [dropdown, setDropdown] = useState(isMobile && !isTablet);
-  
+
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
@@ -31,7 +31,7 @@ const Layout = () => {
       <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z" />
     </svg>
   );
-  
+
   useEffect(() => {
     const anpassa = () => {
       if (window.outerWidth < window.outerHeight) {
@@ -50,33 +50,81 @@ const Layout = () => {
 
   return (
     <>
-      <div className="navbar">
+      <div className={!dropdown ? `navbar` : "bg-white h-[70px]"}>
         {dropdown ? (
-          <div className="mobile-nav">
-            <div
-              className="justify-center flex-row flex items-center"
-              onClick={toggleDropdown}
-            >
-              <div
-                style={{
-                  position: "absolute",
-                  right: "5%",
-                  cursor: "pointer",
-                  rotate: isDropdownOpen && "-90deg",
-                  animationName: isDropdownOpen ? "snurrNer" : "snurrUpp",
-                  animationDuration: "0.1s",
-                }}
-              >
-                {LogoSVG}
+          <div className="">
+            <div className="w-full grid grid-cols-3">
+              <div>ix</div>
+              <div className="flex items-center text-[4vh] justify-center">
+                {
+                  <>
+                    <NavLink
+                      to="/"
+                      className={({ isActive }) =>
+                        isActive ? "active-item" : "hidden"
+                      }
+                    >
+                      {svg}
+                    </NavLink>
+                    <NavLink
+                      to="/Klasser"
+                      className={({ isActive }) =>
+                        isActive ? "active-item" : "hidden"
+                      }
+                    >
+                      Klasser
+                    </NavLink>
+                    <NavLink
+                      to="/Placeringar"
+                      className={({ isActive }) =>
+                        isActive ? "active-item" : "hidden"
+                      }
+                    >
+                      Placeringar
+                    </NavLink>
+                    <NavLink
+                      to="/Klassrum"
+                      className={({ isActive }) =>
+                        isActive ? "active-item" : "hidden"
+                      }
+                    >
+                      Klassrum
+                    </NavLink>
+                    <NavLink
+                      to="/Support"
+                      className={({ isActive }) =>
+                        isActive ? "active-item" : "hidden"
+                      }
+                    >
+                      Hjälp
+                    </NavLink>
+                  </>
+                }
+              </div>
+              <div className="place-self-end w-[70px]">
+                <div className="h-[70px] aspect-square flex items-center justify-center">
+                  <div
+                    onClick={toggleDropdown}
+                    style={{
+                      cursor: "pointer",
+                      rotate: isDropdownOpen && "-90deg",
+                      animationName: isDropdownOpen ? "snurrNer" : "snurrUpp",
+                      animationDuration: "0.1s",
+                    }}
+                  >
+                    {LogoSVG}
+                  </div>
+                </div>
               </div>
             </div>
+
             {isDropdownOpen && (
               <ul className="dropdown-menu">
                 <li className="dropdown-item">
                   <NavLink to="/" onClick={toggleDropdown}>
                     <div
                       style={{ textAlign: "center" }}
-                      className="justify-end flex-row flex items-center"
+                      className="justify-center flex-row flex items-center"
                     >
                       {svg}
                     </div>
@@ -86,7 +134,7 @@ const Layout = () => {
                   <NavLink to="/Placeringar" onClick={toggleDropdown}>
                     <div
                       style={{ textAlign: "center" }}
-                      className="justify-end flex-row flex items-center"
+                      className="justify-center flex-row flex items-center"
                     >
                       Placeringar
                     </div>
@@ -96,7 +144,7 @@ const Layout = () => {
                   <NavLink to="/Klassrum" onClick={toggleDropdown}>
                     <div
                       style={{ textAlign: "center" }}
-                      className="justify-end flex-row flex items-center"
+                      className="justify-center flex-row flex items-center"
                     >
                       Klassrum
                     </div>
@@ -106,7 +154,7 @@ const Layout = () => {
                   <NavLink to="/Klasser" onClick={toggleDropdown}>
                     <div
                       style={{ textAlign: "center" }}
-                      className="justify-end flex-row flex items-center"
+                      className="justify-center flex-row flex items-center"
                     >
                       Klasser
                     </div>
@@ -116,7 +164,7 @@ const Layout = () => {
                   <NavLink to="/Support" onClick={toggleDropdown}>
                     <div
                       style={{ textAlign: "center" }}
-                      className="justify-end flex-row flex items-center"
+                      className="justify-center flex-row flex items-center"
                     >
                       Hjälp
                     </div>
