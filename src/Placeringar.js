756126a63c64ac8e8cc2c03a368fc4a6963f66d6
Centiljard <89267105+Centiljard@users.noreply.github.com>
2024-11-24 20:57:31 +0100

    under

--- src/Placeringar.js
+++ src/Placeringar.js
@@ -195,8 +195,8 @@ const SkapaPlaceringar = () => {
             style={{
               height: visaKlassrumsmeny ? "12rem" : "46px",
               position: visaKlassrumsmeny ? "absolute" : "relative",
-              top: `${klassmenykord[0]}px`,
-              right: `${klassmenykord[1]}px`,
+              top: `${klassrumsmenykord[0]}px`,
+              right: `${klassrumsmenykord[1]}px`,
             }}
             className="overflow-y-scroll w-52 border border-black mt-2"
           >
