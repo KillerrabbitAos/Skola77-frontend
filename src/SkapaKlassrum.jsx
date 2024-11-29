ee09f31dbf51c0130146677d6365d94bd8673726
Bo Gustavsson <bosse@gustavsson.one>
2024-11-29 13:17:34 +0100

    Update SkapaKlassrum.jsx

--- src/SkapaKlassrum.jsx
+++ src/SkapaKlassrum.jsx
@@ -441,7 +441,7 @@ const Grid3 = () => {
       <div
         className={
           vägg ?
-          "m-auto p-5 px-12 w-fit fit-content rounded-lg border-black border-4 mt-4 m-3" : "m-[none] p-[none]"
+          "m-auto p-5 px-12 w-fit fit-content rounded-lg border-black border-4 mt-4 m-3" : "m-[-7px] p-[none]"
         }
       >
         <Klassrum
