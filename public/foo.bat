94916e50bc57123e71b67a8bfb4f0cd0dbb865dd
Bo Gustavsson <bosse@gustavsson.one>
2025-06-09 18:28:11 +0200

    Update foo.bat

--- public/foo.bat
+++ public/foo.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001 >nul
-powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Du är fin som du är :) orm', 'Meddelande')"
+powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Du är fin som du är :)', 'Meddelande')"
